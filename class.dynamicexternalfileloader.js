ce3896016e3c5423e3a8b19d956d1b393ad4e7a8
Mark Howells-Mead <permanent.tourist@gmail.com>
2012-04-06 15:59:44 +0200

    v1.1 Remodelled functions, added alternative methods for usage with jQuery

--- class.dynamicexternalfileloader.js
+++ class.dynamicexternalfileloader.js
@@ -3,7 +3,7 @@ dynamic external file loader
 append javascript or css file to head of html document (call onload)
 
 by mark howells-mead / permanenttourist.ch
-v1.0 / 2012-04-06
+v1.1 / 2012-04-06
 
 usage with e.g. jQuery:
 $(document).read(function(){
@@ -14,33 +14,48 @@ $(document).read(function(){
 
 */
 
+dynamicFileLoader = function(){};// initialize object
 
-//	helper function, applied to every object
-	if(!Object.insertAfter){
-		Object.prototype.insertAfter=function(referenceNode,newNode){
-			referenceNode.parentNode.insertBefore(newNode,referenceNode.nextSibling );
-		}//insertAfter
+dynamicFileLoader.prototype.addScriptFile=function(path){
+	if(path){
+		var htmlTag = document.createElement('script');
+		htmlTag.type = 'text/javascript';
+		htmlTag.async = true;
+		htmlTag.src = path;
+		var head = document.getElementsByTagName ("head")[0] || document.documentElement;
+		head.insertBefore(htmlTag, head.lastChild);
+		return htmlTag;
+	}else{
+		return false;
 	}
+}//addScriptFile
 
-// dynamic file loader
-	dynamicFileLoader = function(){};// initialize object
-	dynamicFileLoader.prototype.addScriptFile=function(path){
-		if(path){
-			var htmlTag = document.createElement('script');
-			htmlTag.type = 'text/javascript';
-			htmlTag.async = true;
-			htmlTag.src = path;
-			var s = document.getElementsByTagName('script');
-			s.insertAfter(s[s.length-1],htmlTag);
-		}
-	}//addScriptFile
-	dynamicFileLoader.prototype.addCSSFile=function(path){
-		if(path){
-			var htmlTag = document.createElement('link');
-			htmlTag.rel = 'stylesheet';
-			htmlTag.type = 'text/css';
-			htmlTag.href = path;
-			var s = document.getElementsByTagName('link');
-			s.insertAfter(s[s.length-1],htmlTag);
-		}
-	}//addCSSFile
+dynamicFileLoader.prototype.addCSSFile=function(path){
+	if(path){
+		var htmlTag = document.createElement('link');
+		htmlTag.rel = 'stylesheet';
+		htmlTag.type = 'text/css';
+		htmlTag.href = path;
+		var head = document.getElementsByTagName("head")[0] || document.documentElement;
+		head.insertBefore(htmlTag, head.lastChild);
+	}
+}//addCSSFile
+
+dynamicFileLoader.prototype.addCSSFileWithJQuery=function(path){
+	if(path){
+		var css = $('head').children(':last');
+		css.attr({
+			rel:  'stylesheet',
+			type: 'text/css',
+			href: path
+		});
+	}
+}//addCSSFileWithJQuery
+
+dynamicFileLoader.prototype.addScriptFileWithJQuery=function(path,onload){
+	if(path && onload){
+		$.getScript(path,onload);
+	}else if(path){
+		$.getScript(path);
+	}
+}//addScriptFileWithJQuery
